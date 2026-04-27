890989731130d584818e3417d816a1ab1fc7f0b4
Lhyunseo03 <82278523+Lhyunseo03@users.noreply.github.com>
2026-04-27 12:16:46 +0900

    gitignore, firebase.js 수정,

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,22 +1,12 @@
 const admin = require('firebase-admin');
 
-let credential;
-if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  const serviceAccount = JSON.parse(
-    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString('utf8')
-  );
-  credential = admin.credential.cert(serviceAccount);
-} else {
-  const serviceAccount = require('../serviceAccountKey.json');
-  credential = admin.credential.cert(serviceAccount);
-}
-
-admin.initializeApp({ credential });
-
-// 토큰 발급 테스트
-admin.app().options.credential.getAccessToken()
-  .then(token => console.log('[DEBUG] 토큰 발급 성공:', token.expirationTime))
-  .catch(err => console.error('[DEBUG] 토큰 발급 실패:', err.message));
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId:   process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey:  process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+});
 
 const db = admin.firestore();
 module.exports = { db };
